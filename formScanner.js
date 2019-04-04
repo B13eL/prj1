1f55ce1d152b47a9069486344603caaca8288093
alex <bosco.bugai@gmail.com>
2019-04-04 22:15:58 +0300

    add README.md, repair JSON, repair time test, add callback,

---
+++ formScanner.js
@@ -0,0 +1,36 @@
+function addEvent(options) {
+    let formList = document.getElementsByTagName("form");
+    for (let i = 0; i < formList.length; i++) {
+        formList[i].addEventListener("submit", function () {
+            validator(formData, options, formList[i].id);
+        })
+    }
+}
+
+function validator(callback, data, id) {
+    console.log('validator');
+    callback(data, id);
+}
+
+function formData(settings, id) {
+    let measuringTimeStart = performance.now(),
+        form = new FormData(document.forms[id]),
+        object = {};
+    form.forEach((value, key) => {
+        object[key] = value;
+    });
+    formData = JSON.stringify(object);
+    let measuringTimeStop = performance.now();
+    console.log(formData + "\n" + (measuringTimeStop - measuringTimeStart));
+    xmlRequest(formData, settings);
+}
+
+function xmlRequest(requestData, options) {
+    for (let fld in options) {
+        this[fld] = options[fld];
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", options.url, true);
+    xhr.setRequestHeader("X-Auth", options.idUser);
+    xhr.send(requestData);
+}
